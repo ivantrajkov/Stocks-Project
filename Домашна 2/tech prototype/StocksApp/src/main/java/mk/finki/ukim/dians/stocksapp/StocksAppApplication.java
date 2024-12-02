6db46681ed747a20bb7520ae9f169a22120958aa
ivantrajkov <139054085+ivantrajkov@users.noreply.github.com>
2024-12-02 22:36:24 +0100

    Add files via upload

---
+++ Домашна 2/tech prototype/StocksApp/src/main/java/mk/finki/ukim/dians/stocksapp/StocksAppApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.dians.stocksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksAppApplication.class, args);
+    }
+
+}
